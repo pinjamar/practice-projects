e5e99b386a415acd0637f3686657f5a7672e5265
Ivan <pinjamar@gmail.com>
2023-04-29 21:07:33 +0200

    hast tables practice

--- _data_structures/hashTables.js
+++ _data_structures/hashTables.js
@@ -69,3 +69,35 @@ myHashTable.get('grapes');
 myHashTable.set('apples', 9);
 myHashTable.set('oranges', 2);
 myHashTable.get('apples');
+
+//given an array = [2,5,1,2,3,5,1,2,4]
+//it should return 2
+//given an array = [2,1,1,2,3,5,1,2,4]
+//it should return 1
+//given an array = [2,3,4,5]
+//it should return undefined
+
+function firstRecurringChar(input) {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[i] === input[j]) {
+        return input[j];
+      }
+    }
+  }
+  return undefined;
+} // O(n2)
+
+function firstRecurringChar2(input) {
+  let map = {};
+  for (let i = 0; i < input.length; i++) {
+    if (map[input[i]] !== undefined) {
+      return input[i];
+    } else {
+      map[input[i]] = i;
+    }
+  }
+  return undefined;
+} //O(n)
+
+firstRecurringChar([2, 5, 1, 2, 3, 5, 1, 2, 4]);
