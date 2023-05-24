2c37399a8ffcb0a8f03326c69e22fa6de8a96476
Ivan <pinjamar@gmail.com>
2023-05-24 02:10:53 +0200

    missing number and number of bits solved

---
+++ _leetcode/missingNumber.js
@@ -0,0 +1,28 @@
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+// Example 1:
+
+// Input: nums = [3,0,1]
+// Output: 2
+// Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+// Example 2:
+
+// Input: nums = [0,1]
+// Output: 2
+// Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+  const n = nums.length;
+  const expectedSum = (n * (n + 1)) / 2;
+  let actualSum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    actualSum = actualSum + nums[i];
+  }
+
+  return expectedSum - actualSum;
+};
