413cd2b6da1b56f40620b223b359e551cf64342c
Ivan <pinjamar@gmail.com>
2023-06-10 19:06:12 +0200

    sqrt and symmetric tree solved

---
+++ _leetcode/syrt.js
@@ -0,0 +1,25 @@
+// Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+// You must not use any built-in exponent function or operator.
+
+// For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+var mySqrt = function (x) {
+  var left = 1;
+  var right = Math.floor(x / 2) + 1;
+  var mid;
+
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+
+    if (mid * mid > x) {
+      right = mid - 1;
+    } else if (mid * mid < x) {
+      left = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+
+  return right;
+};
