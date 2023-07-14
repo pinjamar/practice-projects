162abbcd5516cbb1f19b7181de61e696cda2a670
Ivan <pinjamar@gmail.com>
2023-07-14 12:21:52 +0200

    js & python practice

--- _leetcode/repeating_number.py
+++ _leetcode/repeating_number.py
@@ -1,15 +1,15 @@
 # Given an array of integers find the one that repeats odd number of times
 
 def find_odd_repeating(integers):
-    
+
     # sorted_ints = sorted(list(integers))
-    # print(integers)    
+    # print(integers)
     # print(sorted_ints)
-    
+
     sorted_ints = list(integers)
-    sorted_ints.sort()  
+    sorted_ints.sort()
     print(sorted_ints)
-    
+
     for x in range(0, len(sorted_ints)):
         counter = 0
         for y in range(0, len(sorted_ints)):
@@ -18,6 +18,20 @@ def find_odd_repeating(integers):
         if counter % 2 == 1:
             print(sorted_ints[x])
             break
-    
-    
+
+
 find_odd_repeating([7, 2, 5, 2, 5, 4, 4])
+
+# ALTERNATIVE
+
+# def find_odd_repeating(integers):
+#     sorted_ints = sorted(integers)
+
+#     for test in sorted_ints:
+#         counter = 0
+#         for current in sorted_ints:
+#             if test == current:
+#                 counter += 1
+#         if counter % 2 == 1:
+#             print(test)
+#             break
